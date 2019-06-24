450afccc149519bfbfc91ec1bd6f5cbbe5e48e77
Rahul <rrahul.rajappan@gmail.com>
2019-06-25 04:45:52 +1200

    Added OnTouchEvent

--- app/src/main/java/com/example/flitflight/MainActivity.java
+++ app/src/main/java/com/example/flitflight/MainActivity.java
@@ -2,12 +2,58 @@ package com.example.flitflight;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView scoreLabel;
+    private TextView startLablel;
+    private ImageView box;
+    private ImageView orange;
+    private ImageView pink;
+    private ImageView black;
+
+    //Position
+    private int boxY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scoreLabel = (TextView)findViewById(R.id.scoreLabel);
+        startLablel = (TextView)findViewById(R.id.startLabel);
+        box = (ImageView)findViewById(R.id.box);
+        orange = (ImageView)findViewById(R.id.orange);
+        pink = (ImageView)findViewById(R.id.pink);
+        black = (ImageView)findViewById(R.id.black);
+
+        //Move to out of screen
+        orange.setX(-80);
+        orange.setY(-80);
+        pink.setX(-80);
+        pink.setY(-80);
+        black.setX(-80);
+        black.setY(-80);
+
+        //Temporary
+        startLablel.setVisibility(View.INVISIBLE);
+        boxY = 500;
+
+
     }
+
+    public boolean onTouchEvent(MotionEvent me) {
+
+        if(me.getAction() == MotionEvent.ACTION_DOWN){
+            boxY -= 20;
+        }
+        box.setY(boxY);
+
+        return true;
+    }
+
 }
